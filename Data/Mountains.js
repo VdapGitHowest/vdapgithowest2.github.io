5ea66c6a9587f3b8438687294afda117a2a6593a
VdapGitHowest <peter.van.damme@howest.be>
2021-11-21 11:10:19 +0100

    Add Json data

---
+++ Data/Mountains.js
@@ -0,0 +1,35 @@
+let mountains={
+"Europa":[
+    {
+        name: "Tourmalet",
+        country: "Frankrijk",
+        height: 2115,
+        visited: false
+        },
+        {
+            name: "Matterhorn",
+            country: "zwitserland",
+            height: 3500,
+            visited: true
+            },
+            {
+                name: "Mont Blanc",
+                country: "Frankrijk/Italië",
+                height: 4800,
+                visited: true,
+                warning:"danger" 
+            }],
+            
+            "Buiten Europa":[
+
+            {
+                name: "K2",
+                country: "Pakistan",
+                height: 8600,
+                visited: true,
+                warning:"danger"
+                }
+            ]
+            
+
+        }
